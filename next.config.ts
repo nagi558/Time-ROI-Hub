125cfb54df433a06504821ca0f454ffbbe4f9dad
Koki <glacier.fairytail@gmail.com>
2026-07-09 08:40:12 +0900

    delete/NODE_ENVをdocker-compose.ymlから削除

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from 'next'
-import createMDX from '@next/mdx'
 
-const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-}
+const nextConfig: NextConfig = {}
 
-const withMDX = createMDX({
-  // 必要に応じて、ここにMarkdownプラグインを追加してください
-})
-
-export default withMDX(nextConfig)
+export default nextConfig
